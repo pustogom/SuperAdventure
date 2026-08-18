76b89d16f831e08ab089d2e0f9481c14a32c1c06
Mischa Pustogorodsky <pustogom@gmail.com>
2026-08-17 19:08:54 -0500

    Added the rest of the classes needed to play the RPG.

---
+++ Engine/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Weapon
+    {
+        public int ID {  get; set; }
+        public string Name { get; set; }
+        public string NamePlural { get; set; }
+        public int MaxDamage { get; set; }
+        public int MinDamage { get; set; }
+    }
+}
